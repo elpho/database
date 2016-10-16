2c155d6cf7d35df9c2d9b7fa905e72e73596bf71
Roger Cruz <roger.cruz@ateliware.com.br>
2016-10-16 18:40:39 -0200

    Added entity provider class for dependency injection of entities

---
+++ src/EntityProvider.php
@@ -0,0 +1,29 @@
+<?php
+  namespace elpho\database;
+
+  use elpho\di\DependencyProvider;
+
+  class EntityProvider implements DependencyProvider{
+    private $connection = null;
+    private $entity = null;
+
+    private $entityClass = null;
+    private $entityShortName = null;
+
+    public function __construct(PDO $connection, string $entityClass){
+      $this->connection = $connection;
+      $this->entityClass = $entityClass;
+      $this->entityShortName = basename(str_replace('\\','/',$this->entityClass));
+    }
+
+    public function getProvidedClassName(){
+      return $this->entityShortName;
+    }
+
+    public function getInstance(){
+      if($this->entity == null)
+        $this->entity = new $this->entityClass($this->connection);
+
+      return $this->entity;
+    }
+  }
